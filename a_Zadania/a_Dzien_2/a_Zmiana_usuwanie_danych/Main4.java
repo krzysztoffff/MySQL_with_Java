67937d86c3544d30c09fcd5c25b969d640ecdf89
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-16 09:11:02 +0100

    MySQL updates

--- a_Zadania/a_Dzien_2/a_Zmiana_usuwanie_danych/Main4.java
+++ a_Zadania/a_Dzien_2/a_Zmiana_usuwanie_danych/Main4.java
@@ -5,8 +5,7 @@ public class Main4 {
     //zapisz poniżej zapytania do bazy
     public String query1 = "UPDATE Cinemas SET name='Stadion Narodowy' WHERE name LIKE '%z';";
     public String query2 = "DELETE FROM payments WHERE date < NOW() - INTERVAL 4 DAY;";
-    public String query3 = "";
-    public String query4 = "";
-    public String query5 = "";
+    public String query3 = "UPDATE Movies SET rating = 5.4 WHERE LENGTH(description) > 8;";
+    public String query4 = "UPDATE tickets SET price = (price + price*0.5) WHERE quantity > 10;";
 
 }
