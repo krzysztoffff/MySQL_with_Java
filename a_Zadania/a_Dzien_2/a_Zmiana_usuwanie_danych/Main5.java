52c31fabfb26e4d4bbb670cc982c6cde0ce223d0
krzysztoffff <krzycho@pozaprawem.pl>
2018-02-16 20:03:30 +0100

    edit & delete exercising

--- a_Zadania/a_Dzien_2/a_Zmiana_usuwanie_danych/Main5.java
+++ a_Zadania/a_Dzien_2/a_Zmiana_usuwanie_danych/Main5.java
@@ -1,5 +1,30 @@
 package a_Zadania.a_Dzien_2.a_Zmiana_usuwanie_danych;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main5 {
+
+	public static void main(String[] args) {
+
+		try {
+			Connection connetion = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinemas_ex?useSSL=false",
+					"root", "coderslab");
+			PreparedStatement ps = connetion.prepareStatement("SELECT * FROM CINEMAS;");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				System.out.println(rs.getInt("id") + " | " + rs.getString("name") + " | " + rs.getString("address") );
+			}
+			
+			//TODO Rest of the functionalities
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
